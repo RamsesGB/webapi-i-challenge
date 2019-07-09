cd0af3dab1fcff04b0acee6ac3d2e3a27846ff3c
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2019-07-09 15:24:09 -0400

    create API with CRUD operations

--- index.js
+++ index.js
@@ -4,16 +4,28 @@ const Users = require('./data/db');
 
 const server = express();
 
+server.use(express.json()); // <--- To parse JSON into JS object that can be read in POST function
+
 // The C in CRUD
 server.post('/api/users', (req, res) => {
-  const userData = req.body;
-  Users.insert(userData)
-    .then(users => {
-      res.status(201).json(users);
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+  const { name, bio } = req.body; // Destructuring userData
+
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ errorMessage: 'Please provide name and bio for the user.' });
+  } else {
+    Users.insert(req.body)
+      .then(user => {
+        res.status(201).json(user);
+      })
+      .catch(() => {
+        res.status(500).json({
+          errorMessage:
+            'There was an error while saving the user to the database',
+        });
+      });
+  }
 });
 
 // The R in CRUD
@@ -22,19 +34,76 @@ server.get('/api/users', (req, res) => {
     .then(users => {
       res.status(200).json(users);
     })
-    .catch(error => {
-      res.status(500).json(error);
+    .catch(() => {
+      res.status(500).json({
+        errorMessage: "The users information could not be retrieved."
+      });
+    });
+});
+
+server.get('/api/users/:id', (req, res) => {
+  Users.findById(req.params.id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The user with the specified ID does not exist.' });
+      }
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ errorMessage: 'The user information could not be retrieved.' });
     });
 });
 
 // The U in CRUD
 server.put('/api/users/:id', (req, res) => {
+  const { name, bio } = req.body;
 
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ errorMessage: 'Please provide name and bio for the user.' });
+  } else {
+    Users.update(req.params.id, req.body)
+      .then(user => {
+        if (user) {
+          res.status(200).json(user);
+        } else {
+          res
+            .status(404)
+            .json({
+              message: 'The user with the specified ID does not exist.',
+            });
+        }
+      })
+      .catch(() => {
+        res.status(500).json({
+          errorMessage: 'The user information could not be modified.',
+        });
+      });
+  }
 });
 
 // The D in CRUD
 server.delete('/api/users/:id', (req, res) => {
+  const { id } = req.params;
 
+  Users.remove(id)
+    .then(deleted => {
+      if (deleted) {
+        res.status(204).end();
+      }
+      else {
+        res.status(404).json({ message: 'The user with the specified ID does not exist.' })
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ errorMessage: 'The user could not be removed' });
+    })
 });
 
 const port = 5000;
